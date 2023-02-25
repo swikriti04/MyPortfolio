af35905e9040de8db5a909ed9078c11e5feeeb0c
swikriti04 <swikritikhadke@gmail.com>
2023-02-24 21:50:30 -0500

    Updates

---
+++ precache-manifest.ee99fb65913f4110d3831997821c5fa2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e35e6fe77c9aef7fe7f5",
+    "url": "/MyPortfolio/static/css/main.6ecb5da0.chunk.css"
+  },
+  {
+    "revision": "e35e6fe77c9aef7fe7f5",
+    "url": "/MyPortfolio/static/js/main.e35e6fe7.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/MyPortfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "6311c3affd0fe28667be",
+    "url": "/MyPortfolio/static/js/runtime~main.6311c3af.js"
+  },
+  {
+    "revision": "e080f9808eae5447c889512947099517",
+    "url": "/MyPortfolio/index.html"
+  }
+];
